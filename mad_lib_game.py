d2b690ff6b9b70f36abe7a27a0d2f4174db139c2
prava-coder <96050882+prava-coder@users.noreply.github.com>
2022-02-13 17:51:46 +0530

    Add files via upload

---
+++ mad_lib_game.py
@@ -0,0 +1,7 @@
+#this program is to play mad lib game
+name=input("enter noun:")
+city=input("enter noun:")
+about=input("enter adjectives:")
+
+print("my name is " + name + " i learn the things " + about)
+print("I am from " + city)
